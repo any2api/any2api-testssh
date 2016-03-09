6d93a87bed07304eadcfeaadf658513bd6b52d75
Johannes <jowettinger@gmail.com>
2016-03-09 21:11:41 +0100

    functions added to start/stop server

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var fs = require('fs');
+const fs = require('fs');
+const exec = require('child_process').exec;
 
 module.exports = {
   privateKey: fs.readFileSync('./ssh_id_rsa', 'utf8'),
-  publicKey: fs.readFileSync('./ssh_id_rsa.pub', 'utf8')
+  publicKey: fs.readFileSync('./ssh_id_rsa.pub', 'utf8'),
+  startServer: (name, port, done) => {
+    name = name || 'testssh';
+    port = port || '2222';
+
+    exec(`docker run --name ${name} -d -p ${port}:22 any2api/testssh`, done);
+  },
+  stopServer: (name, done) => {
+    name = name || 'testssh';
+
+    exec(`docker kill ${name} ; docker rm ${name}`, done);
+  }
 };
