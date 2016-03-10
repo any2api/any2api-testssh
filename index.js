abf8c6c8253fea8fe54e914c874115038d98bafa
Johannes <jowettinger@gmail.com>
2016-03-10 08:15:36 +0100

    functions renamed, user & pw added

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const exec = require('child_process').exec;
 module.exports = {
   privateKey: fs.readFileSync('./ssh_id_rsa', 'utf8'),
   publicKey: fs.readFileSync('./ssh_id_rsa.pub', 'utf8'),
-  startServer: (name, port, done) => {
+  username: 'root',
+  password: 'root',
+  startContainer: (name, port, done) => {
     name = name || 'testssh';
     port = port || '2222';
 
     exec(`docker run --name ${name} -d -p ${port}:22 any2api/testssh`, done);
   },
-  stopServer: (name, done) => {
+  stopContainer: (name, done) => {
     name = name || 'testssh';
 
     exec(`docker kill ${name} ; docker rm ${name}`, done);
